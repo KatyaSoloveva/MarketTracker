6baccb991a9405c8cf5149fbee5c152e3666b734
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-05-31 12:33:31 +0000

    an entry point has been created - a file main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+
+from config_data.config import Config, load_config
+
+
+async def main():
+    config: Config = load_config()
+
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling()
+
+asyncio.run(main())
