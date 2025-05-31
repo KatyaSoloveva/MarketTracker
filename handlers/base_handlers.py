3a5ba50c8c105f11126bbae6114e5455bda8d480
Ekaterina Soloveva <KkatSsol@yandex.ru>
2025-05-31 13:32:09 +0000

    LEXICON dict has been created for collecting messages

--- handlers/base_handlers.py
+++ handlers/base_handlers.py
@@ -2,15 +2,16 @@ from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import Command, CommandStart
 
+from lexicon.lexicon import LEXICON
 
 router = Router()
 
 
 @router.message(CommandStart())
 async def start_command_react(message: Message):
-    await message.answer(text='insert later')
+    await message.answer(LEXICON['/start'].format(message.chat.first_name))
 
 
 @router.message(Command(commands='help'))
 async def help_command_reacr(message: Message):
-    await message.answer(text="insert later 2")
+    await message.answer(LEXICON['/help'])
---
+++ lexicon/lexicon.py
@@ -0,0 +1,4 @@
+LEXICON: dict[str: str] = {
+    '/start': 'Здравствуйте, {}! Рад вас видеть! Чем могу быть полезен?',
+    '/help': 'insert later 2'
+}
